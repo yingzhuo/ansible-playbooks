2848525f07b158ad4ee21da4b90266fb24ea49e1
应卓 <yingzhor@gmail.com>
2022-03-20 08:54:41 +0800

    2022-03-20 08:54:41

---
+++ roles/mysql/files/tool-sbin/mysql-check-non-innodb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# 检查mysql实例中不使用"InnoDB"引擎的非系统表
+
+sql="
+  SELECT
+  	TABLE_SCHEMA,
+  	TABLE_NAME,
+  	ENGINE,
+  	FORMAT_BYTES(DATA_LENGTH) AS DATA_SIZE
+  FROM
+  	information_schema.TABLES
+  WHERE
+  	TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')
+  AND
+  	ENGINE <> 'InnoDB';
+"
+
+mysql -e "$sql" | awk '
+{
+  printf("[warning] [non innodb table] database: %s | table: %s | size: %s kB \n", $1, $2, $4)
+}
+'
